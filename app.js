ffd5185f73b26a7cbfce1abdbb121bed525a181c
rafna <rafnaali@gmail.com>
2021-08-09 15:05:28 +0800

    signUp page created

---
+++ app.js
@@ -0,0 +1,9 @@
+ const express = require("express");
+ const bodyParser = require("bodyParser");
+ const request = request("request");
+
+ const app = express();
+
+ app.listen(3000.function(){
+   console.log("server is running on port 3000")
+ });
