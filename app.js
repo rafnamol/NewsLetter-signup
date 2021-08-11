54ba5c28d71c04f84be1f18b1d71d1cc38b646c0
rafna <rafnaali@gmail.com>
2021-08-11 12:21:20 +0800

    activated signup page

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
  const express = require("express");
- const bodyParser = require("bodyParser");
- const request = request("request");
+ const bodyParser = require("body-parser");
+ const request = require("request");
 
  const app = express();
+ app.use (express.static ("public"))
+ app.use (bodyParser.urlencoded({extended:true}));
 
- app.listen(3000.function(){
+ app.get ("/", function(req, res){
+   res.sendFile(__dirname + "/Signup.html");
+ });
+
+ app.post("/",function(req, res){
+
+     var firstName = req.body.fName;
+     var lastName = req.body.lName;
+     var email = req.body.email;
+
+     console.log(firstName, lastName, email);
+
+ });
+
+ app.listen(3000,function(){
    console.log("server is running on port 3000")
  });
